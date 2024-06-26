10513fdcfd7c813581943057fc0a68d5d83c75db
Tracy <tracytangyc@outlook.com>
2024-06-26 13:46:19 +0800

    update ubuntu version concat.sh

--- actions/concat.sh
+++ actions/concat.sh
@@ -1,3 +1,12 @@
-(for %i in (*.MP4) do @echo file '%i') > videolist.txt
+# (for %i in (*.MP4) do @echo file '%i') > videolist.txt
+if [ -z "$1" ]
+then
+    $path=.
+else
+    $path=$1
+fi
+
+find ${path} -type f -name "*.MP4" -exec echo "file "{} \; > videolist.txt
+
 
 ffmpeg -f concat -safe 0 -i videolist.txt -c copy output.MP4
