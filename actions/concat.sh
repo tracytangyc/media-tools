99bda27f7899e94435ab194e6f06c4e69a4b6f4d
Tracy <tracytangyc@outlook.com>
2024-06-26 14:07:38 +0800

    current directory only, sort in filename order, cat and remove txt

--- actions/concat.sh
+++ actions/concat.sh
@@ -6,7 +6,7 @@ else
     $path=$1
 fi
 
-find ${path} -type f -name "*.MP4" -exec echo "file "{} \; > videolist.txt
-
-
-ffmpeg -f concat -safe 0 -i videolist.txt -c copy output.MP4
+find ${path} -maxdepth 1 -type f -name "*.MP4" | sed 's/.*/file &/' | sort | tee videolist.txt
+cat videolist.txt
+ffmpeg -f concat -safe 0 -i videolist.txt -c copy -acodec copy output.MP4
+rm videolist.txt
