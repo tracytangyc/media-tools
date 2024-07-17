a14289f993c49d283746b8ca69ece4613b9a28e6
Tracy Thinkbook <tracytangyc@outlook.com>
2024-07-17 13:00:49 +0800

    switching to ttkbootstrap

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+import ttkbootstrap as ttk
 import functions as f
 
 from components import concat as concat, convert as convert, extract as extract, join_audio_video as joinav, m3u8 as m3u8
 
+root = tk.Tk()
 
-def setup(root:Tk):
-    root.title("音视频处理小工具  Media Tools")
-    root.geometry("960x540")
-
-    label_welcome = ttk.Label(root, text="欢迎使用音视频处理小工具  Media Tools",)
-    label_welcome.pack(pady=20)
-
-    # buttons to choose action
-    btn_action_concat = ttk.Button(root, text="快速拼接", command=concat.setup)
-    btn_action_extract = ttk.Button(root, text="快速截取", command=extract.setup)
-    btn_action_joinav = ttk.Button(root, text="合并音频视频", command=joinav.setup)
-    btn_action_convert = ttk.Button(root, text="格式转换", command=convert.setup)
-    btn_action_m3u8 = ttk.Button(root, text="下载m3u8网络资源", command=m3u8.setup)
-
+root.title("音视频处理小工具  Media Tools")
+root.geometry("960x540")
 
 ## UPPER HALF:
 
@@ -43,10 +32,4 @@ btn_action_m3u8.pack(pady=20)
 frame = ttk.Frame(root)
 frame['padding'] = (20, 20)
 
-
-# MAIN LOOP
-if __name__ == "__main__":
-    win_root = Tk()
-    setup(win_root)
-    win_root.mainloop()
-
+root.mainloop()
--- components/extract.py
+++ components/extract.py
@@ -1,28 +1,28 @@
 import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap as ttk
 
-label_starttime = ttk.Label(root, text="开始时间(HH:MM:SS)：")
-label_starttime.pack(pady=20)
-sv_starttime = tk.StringVar()
-entry_starttime = ttk.Entry(root, textvariable=sv_starttime, validate='key', validatecommand='f.re_hhmmss', width=20)
-entry_starttime.pack(pady=20)
+# label_starttime = ttk.Label(root, text="开始时间(HH:MM:SS)：")
+# label_starttime.pack(pady=20)
+# sv_starttime = tk.StringVar()
+# entry_starttime = ttk.Entry(root, textvariable=sv_starttime, validate='key', validatecommand='f.re_hhmmss', width=20)
+# entry_starttime.pack(pady=20)
 
-label_endtime = ttk.Label(root, text="结束时间(HH:MM:SS)：")
-label_endtime.pack(pady=20)
-sv_endtime = tk.StringVar()
-entry_endtime = ttk.Entry(root, textvariable=sv_endtime, validate='key', validatecommand='f.re_hhmmss', width=20)
-entry_endtime.pack(pady=20)
+# label_endtime = ttk.Label(root, text="结束时间(HH:MM:SS)：")
+# label_endtime.pack(pady=20)
+# sv_endtime = tk.StringVar()
+# entry_endtime = ttk.Entry(root, textvariable=sv_endtime, validate='key', validatecommand='f.re_hhmmss', width=20)
+# entry_endtime.pack(pady=20)
 
-def update_duration():
-    starttime = f.str_to_seconds(sv_starttime.get())
-    endtime = f.str_to_seconds(sv_endtime.get())
-    sv_duration.set(f.seconds_to_str(endtime - starttime))
-    label_duration.config(text="目标时长："+ str(sv_duration))
+# def update_duration():
+#     starttime = f.str_to_seconds(sv_starttime.get())
+#     endtime = f.str_to_seconds(sv_endtime.get())
+#     sv_duration.set(f.seconds_to_str(endtime - starttime))
+#     label_duration.config(text="目标时长："+ str(sv_duration))
 
-button_confirm = ttk.Button(root, text="确认", command=update_duration)
-button_confirm.pack(pady=10)
-button_confirm.state(f.btn_state)
+# button_confirm = ttk.Button(root, text="确认", command=update_duration)
+# button_confirm.pack(pady=10)
+# button_confirm.state(f.btn_state)
 
-sv_duration = tk.StringVar()
-label_duration = ttk.Label(root, text="目标时长：")
-label_duration.pack(pady=20)
+# sv_duration = tk.StringVar()
+# label_duration = ttk.Label(root, text="目标时长：")
+# label_duration.pack(pady=20)
