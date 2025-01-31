79d7938ae07bacdf2df2657187199b14449df630
Tracy Thinkbook <tracytangyc@outlook.com>
2025-01-31 23:04:03 +0800

    backup before laptop fix

--- actions/concat.sh
+++ actions/concat.sh
@@ -1,12 +1,29 @@
-# (for %i in (*.MP4) do @echo file '%i') > videolist.txt
-if [ -z "$1" ]
+# help
+
+if [ -z "$1" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ];
 then
-    $path=.
-else
-    $path=$1
+    echo "Usage: concat.sh [extension] [path]"
+    echo "Concatenates all files of the same extension in the specified path into a single file named concat.[extension]"
+    echo "If no path is provided, the current directory is used"
+    exit 0
 fi
 
-find ${path} -maxdepth 1 -type f -name "*.MP4" | sed 's/.*/file &/' | sort | tee videolist.txt
-cat videolist.txt
-ffmpeg -f concat -safe 0 -i videolist.txt -c copy -acodec copy output.MP4
-rm videolist.txt
+# main
+
+ext="$1"
+echo: "Target extension: $ext"
+
+if [ -z "$2" ];
+then
+    echo "No path provided, using current directory."
+    path=.
+else
+    path="$2"
+fi
+
+echo "Target path: $path"
+
+cd "$path"
+find . -maxdepth 1 -type f -name "*.$ext" | sed "s/.*/file '&'/" | sort | tee concat_list.txt
+ffmpeg -f concat -safe 0 -i concat_list.txt -c copy concat.$ext
+rm concat_list.txt
--- actions/extract.sh
+++ actions/extract.sh
@@ -1,3 +1,21 @@
+# help
+if [ -z "$1" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ];
+then
+    echo "Usage: extract.sh [input] [start] [end]"
+    echo "Extracts a segment from the input file starting and ending at the specified time"
+    echo "Time format: HH:MM:SS"
+    exit 0
+fi
+
+# main
+if [ -z "$2" ] || [ -z "$3" ];
+then
+    echo "Missing start or end time."
+    echo "Usage: extract.sh [input] [start] [end]"
+    echo "Time format: HH:MM:SS"
+    exit 1
+fi
+
 sec1=$(date -d "1970-01-01 $2" +%s)
 sec2=$(date -d "1970-01-01 $3" +%s)
 
@@ -9,6 +27,8 @@ s=$((difference % 3600 % 60))
 
 hhmmss=$(printf "%02d:%02d:%02d\n" $h $m $s)
 
-echo target duration = $hhmmss
+echo "target duration = $hhmmss"
 
-ffmpeg -i $1 -ss $2 -t $hhmmss -acodec copy -c copy "crop $1"
+# input file format: dir/name.ext; output file format: dir/name_extract.ext
+out="${1%.*}_extract.${1##*.}"
+ffmpeg -i "$1" -ss $2 -t $hhmmss -c copy "$out"
